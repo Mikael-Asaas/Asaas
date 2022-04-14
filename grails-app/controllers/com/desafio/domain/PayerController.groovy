54f14747395e7ca40cc638b6b046927d437b373d
Mikael Soares <mikael.soares@asaas.com.br>
2022-04-14 11:14:18 -0300

    Criando o PayerController

---
+++ grails-app/controllers/com/desafio/domain/PayerController.groovy
@@ -0,0 +1,13 @@
+package com.desafio.domain
+
+import grails.validation.ValidationException
+import static org.springframework.http.HttpStatus.*
+import com.desafio.domain.payer.Payer
+
+class PayerController {
+
+    def create(){
+    
+    }
+
+}
