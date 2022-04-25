48c24fdd322563bf80b5c0429a0797625319c73e
Mikael Soares <mikael.soares@asaas.com.br>
2022-04-25 10:13:29 -0300

    Melhorando o def save e adicionando novos def

--- grails-app/services/com/desafio/domain/PayerService.groovy
+++ grails-app/services/com/desafio/domain/PayerService.groovy
@@ -1,28 +1,41 @@
 package com.desafio.domain
-import com.desafio.domain.payer.Payer
 
+import com.desafio.domain.payer.Payer
+import grails.gorm.transactions.Transactional
+
+@Transactional
 class PayerService {
-    def save(Map params){
-        println(params)
-        Payer payer = new Payer()
-        payer.nome = params.nome
-        payer.cpf = params.cpf
-        payer.endereco = params.endereco
-        payer.bairro = params.bairro
-        payer.cidade = params.localidade
-        payer.estado = params.uf
-        payer.numero = params.int("numero")
-        payer.cep = params.int("cep")
-        payer.email = params.email
-        payer.telefone = params.telefone
+
+    def save(Map params) {
+        Payer payer = new Payer(params)
         payer.save(failOnError: true)
     }
 
-    def list(){
+    def list() {
         return Payer.getAll()
     }
 
-    def getPayer(Integer id){
+    def getPayer(Integer id) {
         return Payer.get(id)
     }
+
+    def update(Map params) {
+        if (params.id) {
+            Payer payer = Payer.get(params.int('id'))
+            payer.nome = params.nome
+            payer.cpf = params.cpf
+            payer.endereco = params.endereco
+            payer.bairro = params.bairro
+            payer.cidade = params.cidade
+            payer.estado = params.estado
+            payer.numero = params.numero
+            payer.cep = params.cep
+            payer.email = params.email
+            payer.telefone = params.telefone
+            payer.save(flush: true, failOnError: true)
+       } else {
+            throw new Exception('Erro ao realizar edição')
+        }
+    }
+
 }
