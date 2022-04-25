c0e2f3196cc32cff8cc4cc0bfee098c7775214e7
Mikael Soares <mikael.soares@asaas.com.br>
2022-04-25 10:11:34 -0300

    JavaScript: Função do show hide

---
+++ grails-app/assets/javascripts/payer/payerShow.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  var listaInput = $("input");
+
+  $(".js-edit").on("click", function (e) {
+    e.preventDefault();
+    $(this).addClass("hide");
+    $(".js-send-button").removeClass("hide");
+
+    listaInput.each(function (index, input) {
+      $(input).removeAttr("readonly");
+    });
+  });
+});
