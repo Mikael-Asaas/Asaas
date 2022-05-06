1132746fa2d41ee5973f3cff8c188e5c72b595e7
Mikael Soares <mikael.soares@asaas.com.br>
2022-05-06 14:32:31 -0300

    Melhorando estrutura de pastas

--- grails-app/assets/javascripts/mask.form.js
+++
@@ -1,9 +0,0 @@
-var inputCpfCnpj = $("#cpfcnpj")[0];
-var inputCep = $("#cep")[0];
-var inputTelephone = $("#telephone")[0];
-
-new Inputmask({mask: ["(99) 9999-9999", "(99) 9 9999-9999"], showMaskOnHover: false, keepStatic: true}).mask(inputTelephone);
-new Inputmask({mask: ["999.999.999-99", "99.999.999/9999-99"], showMaskOnHover: false, keepStatic: true}).mask(inputCpfCnpj);
-new Inputmask({mask: "99999-999", showMaskOnHover: false, keepStatic: true}).mask(inputCep);
-
-
