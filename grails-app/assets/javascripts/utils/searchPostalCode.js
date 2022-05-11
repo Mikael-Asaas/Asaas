3da0f7d3abfaff8614dad39e08046b5abad55259
Mikael Soares <mikael.soares@asaas.com.br>
2022-05-11 10:28:18 -0300

    separando as function

--- grails-app/assets/javascripts/utils/searchPostalCode.js
+++ grails-app/assets/javascripts/utils/searchPostalCode.js
@@ -1,9 +1,11 @@
 const validationPostalCode = function(postalCode) {
   return /^[0-9]{8}$/.test(postalCode);
 };
+
 const verificationPostalCode = function(postalCode) {
   return postalCode.replace(/\D/g, "");
 };
+
 const searchPostalCode = async(postalCode, callbackfunction) => {
   if(verificationPostalCode(postalCode).length == 8) {
     let url = `https://viacep.com.br/ws/${postalCode}/json/`;
