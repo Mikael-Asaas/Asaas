ef0120041f031e143edfc118b8032ffe5d7482e7
Mikael Soares <mikael.soares@asaas.com.br>
2022-05-11 12:00:41 -0300

    alterando nomeclatura para ingles

--- grails-app/assets/javascripts/payer/show.js
+++ grails-app/assets/javascripts/payer/show.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-  let listaInput = $("input");
+  let inputList = $("input");
 
   $(".js-edit").on("click", function (e) {
     e.preventDefault();
     $(this).hide();
     $(".js-send-button").removeClass("hide");
 
-    listaInput.each(function (index, input) {
+    inputList.each(function (index, input) {
       $(input).removeAttr("readonly");
     });
   });
