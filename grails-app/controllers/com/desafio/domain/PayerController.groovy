e384b72614c0aa20c39ff82f5aef4a297088aaf2
Mikael Soares <mikael.soares@asaas.com.br>
2022-05-11 15:24:01 -0300

    melhorando o código para fazer apenas uma chamada no bd em vez de duas chamadas

--- grails-app/controllers/com/desafio/domain/PayerController.groovy
+++ grails-app/controllers/com/desafio/domain/PayerController.groovy
@@ -2,6 +2,7 @@ package com.desafio.domain
 
 import com.desafio.domain.payer.Payer
 import grails.converters.JSON
+import grails.gorm.PagedResultList
 
 class PayerController {
 
@@ -10,11 +11,12 @@ class PayerController {
     def create() { }
 
     def index() {
-        return [payerList: Payer.list(max: 10, offset: getCurrentPage()), totalCount: Payer.count()]
+        PagedResultList payerList = Payer.list(max: 10, offset: getCurrentPage())
+        return [payerList: payerList , totalCount: payerList.totalCount]
     }
-
+    
     private Integer getCurrentPage() {
-        if (!params.offset) params.offset = 0
+        if(!params.offset) params.offset = 0
         return Integer.valueOf(params.offset)
     }
 
@@ -22,7 +24,7 @@ class PayerController {
         try {
             Payer payer = payerService.save(params)
 
-            if (payer.hasErrors()) {
+            if(payer.hasErrors()) {
                 render([success: false, message: message(code: payer.errors.allErrors[0].defaultMessage ?: payer.errors.allErrors[0].codes[0])] as JSON)
                 return
             }
