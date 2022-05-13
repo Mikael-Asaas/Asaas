e5e24399530100aea17c1ff949b112e13bf0ef26
Mikael Soares <mikael.soares@asaas.com.br>
2022-05-13 11:42:04 -0300

    removendo a função clearForm

--- grails-app/assets/javascripts/utils/fillFormPostalCode.js
+++ grails-app/assets/javascripts/utils/fillFormPostalCode.js
@@ -1,6 +1,5 @@
 
 document.getElementById("postalCode").addEventListener("input", function() {
-  clearForm();
   if(validationPostalCode && verificationPostalCode(this.value)) {
     searchPostalCode(this.value, fillOutForm);
   };
@@ -11,12 +10,4 @@ function fillOutForm(location) {
   document.getElementById("province").value = location.bairro;
   document.getElementById("city").value = location.localidade;
   document.getElementById("state").value = location.uf;
-};
-
-function clearForm() {
-  document.getElementById("address").value = "";
-  document.getElementById("province").value = "";
-  document.getElementById("city").value = "";
-  document.getElementById("state").value = "";
-  document.getElementById("addressNumber").value = "";
 };
