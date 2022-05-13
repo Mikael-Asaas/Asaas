90de255e3d95ed91a67192f0013dff81e91ecf83
MANO EH ELA <manoela.cunha@asaas.com.br>
2022-05-13 15:56:55 -0300

    Restrurando código

--- grails-app/assets/javascripts/customer/customerSave.js
+++ grails-app/assets/javascripts/customer/customerSave.js
@@ -1,42 +1,16 @@
 $(document).ready(function() {
-    $("form").on("submit", function(event) {
-      event.preventDefault();
-  
-      var data = new FormData(document.querySelector("form"));
-      var customer = {};
-  
-      data.forEach(function(value, key) {
-        customer[key] = value;
-      });
-  
-      var url = $('form').prop('action')
+  $("form").on("submit", function(event) {
     
-      $.post(url, customer, function(response) {
-         if (response.success) {
-         window.location.href = $('form').data('url-redirect');
-        }
-      });
-    });
+    event.preventDefault();
 
-    document.getElementById("postalCode").addEventListener("input", function() {
-      clearForm();
-      if (validationPostalCode(this.value)) {
-         searchPostalCode(this.value, fillOutForm)
+    var formReference = $("form");
+    var url = formReference.data("url");
+    var params = formReference.serialize();
+
+    $.post(url, params, function(response) {
+      if(response.success) {
+        window.location.href = formReference.data("redirect");
       }
-    });
-
-    function fillOutForm(location) {
-      document.getElementById("address").value = location.logradouro;
-      document.getElementById("province").value = location.bairro;
-      document.getElementById("city").value = location.localidade;
-      document.getElementById("state").value = location.uf;
-    };
-    
-    function clearForm() {
-      document.getElementById("address").value = "";
-      document.getElementById("province").value = "";
-      document.getElementById("city").value = "";
-      document.getElementById("state").value = "";
-      document.getElementById("addressNumber").value = "";
-    };
-  });
+    })
+  })
+})
