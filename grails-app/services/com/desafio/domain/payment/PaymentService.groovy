5eb0d3358ccbe37c788ffaa479c2dc3e60c1ce11
Mikael Soares <mikael.soares@asaas.com.br>
2022-05-20 12:57:07 -0300

    Adicionando os métodos value, status, method e dueDate

--- grails-app/services/com/desafio/domain/payment/PaymentService.groovy
+++ grails-app/services/com/desafio/domain/payment/PaymentService.groovy
@@ -3,6 +3,9 @@ package com.desafio.domain
 import com.desafio.domain.payment.Payment
 import com.desafio.domain.payer.Payer
 import com.desafio.domain.customer.Customer
+import com.desafio.enums.PaymentMethod
+import com.desafio.enums.PaymentStatus
+import com.desafio.utils.FormatDateUtils
 
 import grails.gorm.transactions.Transactional 
 
@@ -10,7 +13,11 @@ import grails.gorm.transactions.Transactional
 class PaymentService {
 
     public Payment save(Map params) {
-        Payment payment = new Payment(params)
+        Payment payment = new Payment()
+        payment.value = params.value
+        payment.status = PaymentStatus.PENDING
+        payment.method = PaymentMethod.valueOf(params.method)
+        payment.dueDate = FormatDateUtils.toDate(params.dueDate, "yyyy-mm-dd")
         payment.customer = Customer.get(params.long("customerId"))
         payment.payer = Payer.get(params.long("payerId"))
         payment.save(failOnError: true)
