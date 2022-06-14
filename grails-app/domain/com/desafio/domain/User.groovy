3fae51e39f0a59cbd3c16af1ff89ce5883142dfa
Mikael Soares <mikael.soares@asaas.com.br>
2022-06-13 23:07:14 -0300

    identação

--- grails-app/domain/com/desafio/domain/User.groovy
+++ grails-app/domain/com/desafio/domain/User.groovy
@@ -7,15 +7,21 @@ import grails.compiler.GrailsCompileStatic
 @GrailsCompileStatic
 @EqualsAndHashCode(includes='username')
 @ToString(includes='username', includeNames=true, includePackage=false)
+
 class User implements Serializable {
 
     private static final long serialVersionUID = 1
 
     String username
+
     String password
+
     boolean enabled = true
+
     boolean accountExpired
+
     boolean accountLocked
+
     boolean passwordExpired
 
     Set<Role> getAuthorities() {
